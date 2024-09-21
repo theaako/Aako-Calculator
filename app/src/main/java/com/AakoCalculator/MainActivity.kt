518c65b7c2b3379740e672cd615ba8e6028fac88
Aako <aakoalipour@gmail.com>
2024-09-21 14:14:21 +0330

    A debug and first run: Technical things were OK and all of button(but clear all, enter and erase left) work right.

--- app/src/main/java/com/AakoCalculator/MainActivity.kt
+++ app/src/main/java/com/AakoCalculator/MainActivity.kt
@@ -2,6 +2,7 @@ package com.AakoCalculator
 
 import android.os.Bundle
 import android.view.View
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.AppCompatButton
@@ -10,6 +11,8 @@ import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
     private val input = mutableListOf<String>()
+    private var resultTextBox: TextView? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -19,8 +22,12 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        resultTextBox = findViewById(R.id.resultTextView)
     }
+
     fun onClick(button: View) {
         input.add((button as AppCompatButton).text.toString())
+        resultTextBox?.text = "${resultTextBox?.text} ${button.text}"
     }
 }
